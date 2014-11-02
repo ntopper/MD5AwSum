b3a1b2a607c28bfacded3bfe73a32a1f84fcfffc
ntopper <nicktopper@gmail.com>
2014-11-02 16:31:56 -0500

    formatting

--- rainbow_table.cpp
+++ rainbow_table.cpp
@@ -4,71 +4,70 @@
 using namespace std;
 
 RainbowTable::RainbowTable() {
-	//initalize file_path using config class
-	string f = "tests/tst.xml";
-	this->file_path = f;
+    //initalize file_path using config class
+    string f = "tests/tst.xml";
+    this->file_path = f;
 }
 
 const int RainbowTable::ERROR_READING_FILE = 0;
 
 void RainbowTable::parse() throw(int) {
-	
-	const char * c = this->file_path.c_str();
-	if(!this->tree.load_file(c)) {
-		throw(ERROR_READING_FILE);
-	}
+
+    const char * c = this->file_path.c_str();
+    if(!this->tree.load_file(c)) {
+        throw(ERROR_READING_FILE);
+    }
 }
 
 void RainbowTable::search(string &hash) {
-	pugi::xml_node root = this->tree.child("RainbowTable");
+    pugi::xml_node root = this->tree.child("RainbowTable");
 
-	//search through child nodes
-	for (pugi::xml_node entry = root.first_child(); 
-			entry; 
-			entry = entry.next_sibling()) {
+    //search through child nodes
+    for (pugi::xml_node entry = root.first_child();
+            entry;
+            entry = entry.next_sibling()) {
 
-		const char * entry_hash = entry.attribute("hash").value();
-		
-    	//is this a match?
-    	if(strcmp(entry_hash, hash.c_str()) == 0) {
-    		RainbowTable::print_entry(entry);
-    	} 
-	}
+        const char * entry_hash = entry.attribute("hash").value();
 
+        //is this a match?
+        if(strcmp(entry_hash, hash.c_str()) == 0) {
+            RainbowTable::print_entry(entry);
+        }
+    }
 }
 
 void RainbowTable::print_entry(pugi::xml_node &entry) {
-	//prints all data associated with node entry
+    //prints all data associated with node entry
 
-	cout << endl;//redability
+    cout << endl;//redability
 
-	//print attributes of entry node
-    for (pugi::xml_attribute attr = entry.first_attribute(); 
-    		attr; 
-    		attr = attr.next_attribute()) {
+    //print attributes of entry node
+    for (pugi::xml_attribute attr = entry.first_attribute();
+            attr;
+            attr = attr.next_attribute()) {
 
-    	if (strcmp("key" , attr.name()) != 0 ) { //ignore key
-    		cout << attr.name() << ": " << attr.value() << endl;
-    	}
+        if (strcmp("key" , attr.name()) != 0 ) { //ignore key
+            cout << attr.name() << ": " << attr.value() << endl;
+        }
     }
 
-	//search through child nodes
-	for (pugi::xml_node child = entry.first_child(); 
-			child;
-			child = child.next_sibling()) {
-			
-			cout  << child.name() << endl;  
+    //search through child nodes
+    for (pugi::xml_node child = entry.first_child();
+            child;
+            child = child.next_sibling()) {
 
-			//is there text outside of the attrubutes?
-			if (child.text()) {
-				cout << '\t'  << child.first_child().value() << endl;
-			}
+        cout  << child.name() << endl;
 
-			//print attributes of child
-		    for (pugi::xml_attribute attr = child.first_attribute(); 
-		    		attr; 
-		    		attr = attr.next_attribute()) {
-				cout << '\t' << attr.name() << ": " << attr.value() << endl;
-		    	}	    
+        //is there text outside of the attrubutes?
+        if (child.text()) {
+            cout << '\t'  << child.first_child().value() << endl;
+        }
+
+        //print attributes of child
+        for (pugi::xml_attribute attr = child.first_attribute();
+                attr;
+                attr = attr.next_attribute()) {
+            cout << '\t' << attr.name() << ": " << attr.value() << endl;
+        }
     }
 }
--- rainbow_table.h
+++ rainbow_table.h
@@ -1,6 +1,6 @@
 #ifndef RAINBOW_TABLE_H
 #define RAINBOW_TABLE_H
- 
+
 #define PUGIXML_HEADER_ONLY
 #include "pugixml.cpp"
 #include "pugixml.hpp"
@@ -10,24 +10,24 @@ using namespace std;
 
 class RainbowTable {
 
-	public:
-		RainbowTable();
-		RainbowTable(const string &path) : file_path(path) {}
+public:
+    RainbowTable();
+    RainbowTable(const string &path) : file_path(path) {}
 
-		void parse() throw(int);
-		void search(string &hash);
-		void add(string &file_path, string &key);
-		void remove(string &file_path, string &key);
-		void write();
+    void parse() throw(int);
+    void search(string &hash);
+    void add(string &file_path, string &key);
+    void remove(string &file_path, string &key);
+    void write();
 
-		//errors
-		const static int ERROR_READING_FILE;
+    //errors
+    const static int ERROR_READING_FILE;
 
-	private:
-		string file_path;
-		pugi::xml_document tree;
+private:
+    string file_path;
+    pugi::xml_document tree;
 
-		static void print_entry(pugi::xml_node &entry);
+    static void print_entry(pugi::xml_node &entry);
 };
 
 #endif
