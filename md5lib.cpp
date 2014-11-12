5e28f1ab0761b1e8b47678bb4af980b421a32994
Mark Turiansky <mark.turiansky@yahoo.com>
2014-11-12 00:12:47 -0500

    laying foundation for md5lib

--- md5lib.cpp
+++ md5lib.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <stdio.h>
+#include <math.h>
 
 #define HASHERROR 1
 
@@ -21,4 +22,29 @@ string md5lib::hash(string file_path) throw(int) {
 
 	//this function will have the calls to the various parts
 	//of the md5 algorithm
+}
+
+void md5lib::initialize() {
+	this->s = {7,12,17,22,7,12,17,22,7,12,17,22,7,12,17,22,5,9,14,20,5,9,14,20,5,9,14,20,5,9,14,20,4,11,16,23,4,11,16,23,4,11,16,23,4,11,16,23,6,10,15,21,6,10,15,21,6,10,15,21};
+	for(int i = 0; i < 64; i++) {
+		this->K[i] = floor(abs(sin(i+1)) * pow(2,32));
+	}
+
+	this->a0 = 0x67452301;
+	this->b0 = 0xefcdab89;
+	this->c0 = 0x98badcfe;
+	this->d0 = 0x10325476;
+}
+
+void md5lib::preprocess() {
+	//preprocessing of input
+	//padding
+}
+
+void md5lib::digest() {
+	//main processing of input
+}
+
+void md5lib::finalize() {
+	//put it all together
 }
--- md5lib.h
+++ md5lib.h
@@ -2,6 +2,15 @@
 using namespace std;
 
 class md5lib {
+	private:
+		int[64] s;
+		int[64] K;
+		int a0, b0, c0, d0;
+
 	public:
 		static string hash(string) throw(int);
+		static void initialize();
+		static void preprocessing(); // these will be modified either to be not static
+		static void digest(); // or to take in parameters
+		static void finalize(); // should probably be instantiation but idk
 };
