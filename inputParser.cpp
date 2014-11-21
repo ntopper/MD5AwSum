3920ab3fcc225c11c86cc3aa2f9bc5c4741279b0
Mark Turiansky <mark.turiansky@yahoo.com>
2014-11-21 01:07:51 -0500

    something weird happened in the inputParser, fixed

--- inputParser.cpp
+++ inputParser.cpp
@@ -9,10 +9,7 @@ using namespace std;
 #define UPDATE 3
 #define HELP 4
 #define BAD_INPUT 5
-<<<<<<< HEAD
 #define CHECKSOME 6
-=======
->>>>>>> e0cf53d6d397456ff1627001f95336db16728cd8
 
 /*
  * argv[0] is the name of the program
@@ -26,7 +23,6 @@ using namespace std;
 class inputParser{
 	public:
 		static int parseInput(int argc, char* argv[]){
-<<<<<<< HEAD
 			if(!strcmp(argv[1], "--lookup") || !strcmp(argv[1], "-l")) {
 				if(argc != 3) return BAD_INPUT;			
 				return LOOKUP;
@@ -44,23 +40,6 @@ class inputParser{
 				return HELP;
 			} else if (argc == 2){
 				return CHECKSOME;
-			} else return BAD_INPUT;
-=======
-			if(strcmp(argv[1], "--lookup") || strcmp(argv[1], "-l")) {
-				if(argc != 3) return BAD_INPUT;			
-				return LOOKUP;
-			} else if(strcmp(argv[1], "--add") || strcmp(argv[1], "-a")) {
-				if(argc != 3) return BAD_INPUT;			
-				return ADD;
-			} else if(strcmp(argv[1], "--remove") || strcmp(argv[1], "-r")){
-				if(argc != 3) return BAD_INPUT;
-				return REMOVE;
-			} else if(strcmp(argv[1], "--update") || strcmp(argv[1], "-u")){
-				if(argc != 3) return BAD_INPUT;
-				return UPDATE;
-			} else if(strcmp(argv[1], "--help") || strcmp(argv[1], "-h")){
-				if(argc != 3) return BAD_INPUT;
-				return HELP;
 			} else return BAD_INPUT;
 		}
 
@@ -80,7 +59,6 @@ class inputParser{
 			cout << "\t-a, --add\t\tadd text" << endl;
 			cout << "\t-r, --remove\t\tremove text" << endl;
 			cout << "\t-u, --update\t\tupdate text" << endl;
->>>>>>> e0cf53d6d397456ff1627001f95336db16728cd8
 		}
 };
 
